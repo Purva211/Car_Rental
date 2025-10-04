9e440803c94187d4c74f35f2dc2d6f259ebde9f1
purva <purvadev2@gmail.com>
2025-10-04 20:27:58 +0530

    add Hero page

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center'>
+       <h1 className='text-4xl md:text-5xl font-semibold'>Luxury cars on Rent</h1>
+    </div>
+  )
+}
+
+export default Hero

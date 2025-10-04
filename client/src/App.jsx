273404e8eda2a41668534412a1e04f3732b3dfd0
purva <purvadev2@gmail.com>
2025-10-04 20:33:10 +0530

    route all the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,27 @@
 
 //rafce (to get boilerplate)
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes,useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import CarDetails from './pages/CarDetails'
+import MyBookings from './pages/MyBookings'
 
 const App = () => {
+  const [ShowLogin, setShowLogin] = useState(false)
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App

1c399ce09fa2f29afd2c701008676d4a795d2a11
purva <purvadev2@gmail.com>
2025-10-09 00:38:02 +0530

    add db changes

--- server/configs/db.js
+++ server/configs/db.js
@@ -12,14 +12,14 @@ const connectDB = async () => {
     });
 
     mongoose.connection.on("connected", () => {
-      console.log("✅ Database is connected");
+      console.log("Database is connected");
     });
 
     mongoose.connection.on("error", (err) => {
-      console.error("❌ Database connection error:", err);
+      console.error(" Database connection error:", err);
     });
   } catch (error) {
-    console.error("❌ Database connection failed:", error.message);
+    console.error(" Database connection failed:", error.message);
     process.exit(1); // Stop the app if DB fails
   }
 };
